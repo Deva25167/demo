79600541b9c068fda9dbf9256967c35a11e2fdd9
Prabhudeva R <prabhu25d@gmail.com>
2025-07-24 09:40:00 +0530

    Greatestofthree.java

--- Greatestofthree.java
+++ Greatestofthree.java
@@ -1,1 +1,21 @@
 # It is basics Java program which its compare the three numbers and find the greatest number amoung the three number
+
+
+  
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 3 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        System.out.println("Largest number is: " + max);
+    }
+}
